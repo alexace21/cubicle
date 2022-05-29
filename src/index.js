b283abb28a693062c8e4691d46fe6db88cd495ab
alexace21 <alex.ace7ov@gmail.com>
2022-05-29 18:13:53 +0300

    Render home page & organize App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+})); // hbs is the extension as well
+
+app.set('view engine', 'hbs'); // change the view engine
+app.set('views', './src/views'); // give the hbs the path to views // fixed in package json node start
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.listen(5000, () => console.log('App is listening on port 5000....'));
