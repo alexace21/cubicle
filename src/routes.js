d435df5b22e349cc37ab6c814d303abc921e4b03
alexace21 <alex.ace7ov@gmail.com>
2022-05-29 19:54:54 +0300

    Fix homeController middleware and add urlEncoded middleware

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,10 @@ const homeController = require('./controllers/home');
 const router = express.Router(); // create custom router
 const cubeController = require('./controllers/cube');
 
-router.get('/', homeController.index); // give action for specific path URL
-router.get('/about', homeController.about);
+router.use('/', homeController);
+//router.get('/', homeController.index); // give action for specific path URL
+//router.get('/about', homeController.about); // 1st Way to do Router
 
-router.use('/cube', cubeController); // give Controller/router to take action for this specific path URL
+router.use('/cube', cubeController); // 2nd Way to do Router give Controller/router to take action for this specific path URL
 
 module.exports = router;
