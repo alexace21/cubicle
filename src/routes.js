45ee1c452cade6401732964961181a3c36ac20ac
alexace21 <alex.ace7ov@gmail.com>
2022-05-29 19:10:39 +0300

    Add about page and fix navigation

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const homeController = require('./controllers/home');
 const router = express.Router(); // create custom router
+const cubeController = require('./controllers/cube');
 
 router.get('/', homeController.index); // give action for specific path URL
+router.get('/about', homeController.about);
+
+router.use('/cube', cubeController);
 
 module.exports = router;
