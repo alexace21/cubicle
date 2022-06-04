aea8ebb887c49625b5a525ad1f5a75fb78e340e4
alexace21 <alex.ace7ov@gmail.com>
2022-06-04 21:37:47 +0300

    Fix accessory and cube business logic and functionality

--- src/controllers/cube.js
+++ src/controllers/cube.js
@@ -4,7 +4,7 @@ const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('create');
-})
+});
 
 router.post('/create', (req, res) => {
     const cube = req.body;
@@ -22,19 +22,29 @@ router.post('/create', (req, res) => {
         .catch(err => {
             res.status(400).send('Bad Request');
         })
-})
+});
 
 router.get('/details/:id', async (req, res) => {
     const cube = await cubeService.getOne(req.params.id).lean();
 
     res.render('details', { cube });
-})
+});
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId).lean();
     const accessories = await accessoryService.getAll().lean();
 
     res.render('accessory/attach', {cube, accessories});
-})
+});
+
+router.post('/:cubeId/attach-accessory', async (req, res) => {
+    const accessoryId = req.body.accessory;
+
+    console.log(accessoryId);
+
+    await cubeService.attachAccessory(req.params.cubeId, accessoryId);
+
+    res.redirect(`/cube/details/${req.params.cubeId}`)
+});
 
 module.exports = router;
