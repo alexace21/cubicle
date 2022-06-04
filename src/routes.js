bdee274d9d624ed846071d79755bd76b4de16cd5
alexace21 <alex.ace7ov@gmail.com>
2022-06-04 20:30:44 +0300

    Add accessory page

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const homeController = require('./controllers/home');
 const router = express.Router(); // create custom router
 const cubeController = require('./controllers/cube');
+const accessoryController = require('./controllers/accessoryController');
 
 router.use('/', homeController);
 //router.get('/', homeController.index); // give action for specific path URL
 //router.get('/about', homeController.about); // 1st Way to do Router
-
+router.use('/accessory', accessoryController);
 router.use('/cube', cubeController); // 2nd Way to do Router give Controller/router to take action for this specific path URL
 
 module.exports = router;
